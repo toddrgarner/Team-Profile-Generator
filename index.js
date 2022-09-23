f3aee7f18da4c38235a3e76a1807f21f8c3e1fd3
toddrgarner <110719370+toddrgarner@users.noreply.github.com>
2022-09-23 09:25:24 -0700

    updated index.js

--- index.js
+++ index.js
@@ -1,98 +1,119 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const renderEmployeeCards = require("./src/generateHTML")[0];
+const Manager = require("./lib/manager");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+// const renderEmployeeCards = require("./src/generateHTML");
 
-// an empty array where the questions will get pushed 
+// an empty array where the questions will get pushed
 const employeeArray = [];
 
-const questions = [
-    {
-        type: "input", 
-        name: "name", 
-        message: "what is your name?"
-    },
-    {
-        type: "input", 
-        name: "id", 
-        message: "what is your employee id?"
-    },
-    {
-        type: "input", 
-        name: "email", 
-        message: "what is your email?",
+
+const managerQuestions = () => {
+    return inquirer.prompt([{
+        
+    }])
+
+   
+}
+
+
+const employeeQuestions = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "role",
+        message: "What is this employee's role?",
+        choices: ["Engineer", "Intern"],
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "Enter employee name",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "Enter employee id",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter employee email address",
         validate: function (input) {
-            { 
-                // Regex mail check (return true if valid mail)
-                validEmail = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(input);
+          {
+            // Regex mail check (return true if valid mail)
+            validEmail =
+              /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(
+                input
+              );
 
-                if(validEmail){
-                    return true;
-                } else {
-                    console.log(" --- Please enter a valid email address ---");
-                    return false;
-                }
+            if (validEmail) {
+              return true;
+            } else {
+              console.log(" --- Please enter a valid email address ---");
+              return false;
             }
-        }
-    },
-    {
-        type: "input", 
-        name: "officeNumber", 
-        message: "what is your office number?"
-    },
-    {
-        type: "list", 
-        name: "typeEmployee", 
-        message: "what type of employee would you like to add?",
-        choices: ["Engineer","Intern","No More Employees"],
-    },
-]
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Enter manager's office number",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "Enter engineer github",
+        when: (input) => input.role === "Engineer",
+      },
+      {
+        type: "input",
+        name: "intern",
+        message: "Enter intern school",
+        when: (input) => input.role === "Intern",
+      },
 
-// function to use inquirer to get user input at start
-async function init() {
-    let questioning = true;
+      {
+        type: "confirm",
+        name: "confirmAddEmployee",
+        message: "do you want to add another employee?",
+        default: false,
+      },
+    ])
+    .then((employeeData) => {
+      const { name, id, email, role, github, school, confirmAddEmployee } =
+        employeeData;
+      let employee;
+      if (role === "Engineer") {
+        employee = new Engineer(name, id, email, github);
+        console.log(employee);
+      } else if (role === "Intern") {
+        employee = new Intern(name, id, email, school);
+        console.log(employee);
+      }
+      employeeArray.push(employee);
+      if (confirmAddEmployee) {
+        return employeeQuestions(employeeArray);
+      } else {
+        return employeeArray;
+      }
+    });
+};
+employeeQuestions();
 
-    while (questioning) {
-        const answers = await inquirer.prompt (questions)
+//const manager = new Manager(name, id, email, officeNumber);
+//employeeArray.push(manager)
+//console.log(employeeArray)
 
-        // if user chooses "no more employees" set quesioning to false and stop the loop
-        if (answers.typeEmployee === "No More Employees") {
-            questioning = false;
-        }
+// // function to create html file for team
+// function writeToFile(fileName, data) {
+//     fs.writeFile(fileName, data, (err) =>
+//         (err) ? console.log("error") : console.log("HTML File Generated")
+//     )
+// }
 
-        // if this, then replace question about office number with question about github
-        if (answers.typeEmployee === "Engineer") {
-            questions.splice(3, 1, {
-                type: "input", 
-                name: "github", 
-                message: "what is your github username?",
-            })
-        }
-        
-        // if this, then replace question about office number with question about school
-        if (answers.typeEmployee === "Intern") {
-            questions.splice(3, 1, {
-                type: "input", 
-                name: "school", 
-                message: "what is the name of your school?",
-            })
-        }
-
-        // push the object with the answers to the empty employeeArray
-        employeeArray.push(answers);
-        
-    }
-
-    const htmlOutput = renderEmployeeCards(employeeArray)
-    writeToFile("./dist/team.html", htmlOutput)
-    
-}
-
-// function to create html file for team
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
-        (err) ? console.log("error") : console.log("HTML File Generated")
-    )
-}
-
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init()
